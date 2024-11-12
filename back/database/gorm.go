e7add110b7b424cbc5c93f1d13826113792b4444
Kate0418 <nakao.keito0418@gmail.com>
2024-11-12 23:22:22 +0900

    env

---
+++ back/database/gorm.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func Gorm() *gorm.DB {
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbCharset := os.Getenv("DB_CHARSET")
+	dbParseTime := os.Getenv("DB_PARSE_TIME")
+	dbLoc := os.Getenv("DB_LOC")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%s&loc=%s",
+		dbUser, dbPassword, dbHost, dbPort, dbName, dbCharset, dbParseTime, dbLoc)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+
+	return db
+}
